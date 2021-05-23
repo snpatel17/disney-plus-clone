1f2a0f586e7f011c953b514b72efcb7a0d0cb7f7
snpatel17 <35784647+snpatel17@users.noreply.github.com>
2021-05-23 11:42:00 -0400

    Header component react.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,33 @@ function Header() {
   return (
     <Nav>
       <Logo src="images/logo.svg" />
-      <NavMenu></NavMenu>
+      <NavMenu>
+        <a>
+          <img src="images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="images/original-icon.svg" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="images/userprofile-img.jpg"></UserImg>
     </Nav>
   );
 }
@@ -16,7 +42,7 @@ const Nav = styled.nav`
   height: 70px;
   background: #090b13;
   display: flex;
-  aling-items: centre;
+  align-items: center;
   padding: 0 36px;
 `;
 
@@ -24,4 +50,29 @@ const Logo = styled.img`
   width: 80px;
 `;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  aling-items: center;
+  a {
+    align-items: center;
+    display: flex;
+    padding: 0 12px;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
