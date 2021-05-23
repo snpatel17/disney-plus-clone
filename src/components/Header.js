e5309902f2df7063b58bc0ced31e76970d55724a
snpatel17 <35784647+snpatel17@users.noreply.github.com>
2021-05-23 11:18:52 -0400

    import images and display logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Header() {
-  return <Container>header</Container>;
+  return (
+    <Nav>
+      <Logo src="images/logo.svg" />
+      <NavMenu></NavMenu>
+    </Nav>
+  );
 }
 
 export default Header;
 
-const Container = styled.div`
+const Nav = styled.nav`
   height: 70px;
   background: #090b13;
+  display: flex;
+  aling-items: centre;
+  padding: 0 36px;
 `;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div``;
